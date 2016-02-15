284aa89bbd1f150a15b28c7ee2ca079ab5c225d6
Vivek Choksi <viv.choksi@gmail.com>
2016-02-14 20:42:43 -0800

    Add comments and TODOs

--- scripts/run_baseline.py
+++ scripts/run_baseline.py
@@ -7,6 +7,11 @@
 # run_baseline.exp.
 #
 # This script should be run from within screen on a corn server.
+# > ssh SUNetID@corn.stanford.edu
+# > cd path/to/repo/scripts
+# > screen
+# > python run_baseline.py
+# > # You can press ctrl-d to detach from screen and ctrl-r to re-attach.
 
 import os
 import time
@@ -18,10 +23,10 @@ def get_server_number(counter):
 # Keeps track of which corn server to use.
 counter = 0
 
-for i in xrange(3):
-  # Can add more parameters to the run_baseline.exp call as necessary.
-  parameters = "..." # Placeholder.
+# TODO(arushir): Modify this for loop to loop over appropriate parameters.
+# These parameters will be passed to cnn-baseline.py.
+for parameters in ['...', '...', '...']:
   os.system("/usr/bin/expect -f run_baseline.exp %s %s &"
     % (get_server_number(counter), parameters))
   counter += 1
-  time.sleep(10)
+  time.sleep(5)
