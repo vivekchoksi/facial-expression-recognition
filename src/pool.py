Fix max pooling errors; next to verify if backpropagation needs fixing

src/pool.py
@@ -1,4 +1,5 @@
 from keras.layers.convolutional import *
+from keras.layers.convolutional import _Pooling2D
 from keras import backend as K
 import numpy as np
 import pdb
@@ -9,24 +10,17 @@ import theano.tensor as T
 
 _srng = T.shared_randomstreams.RandomStreams()
 
-class _Pooling2D2(Layer):
-    '''Abstract class for different pooling 2D layers.
+def theano_shuffled(input):
+    n = input.shape[0]
+    shuffled = T.permute_row_elements(input.T, _srng.permutation(n=n)).T
+    return shuffled
+
+def fractional_conv_output_length(input_length, filter_size, stride):
+    return int(np.ceil(input_length / (filter_size * stride)))
+
+class _FractionalPooling2D(_Pooling2D):
+    '''Abstract class for fractional max-pooling 2D layer.
     '''
-    input_ndim = 4
-
-    def __init__(self, pool_size=(2, 2), strides=None, border_mode='valid',
-                 dim_ordering='th', **kwargs):
-        super(_Pooling2D2, self).__init__(**kwargs)
-        self.input = K.placeholder(ndim=4)
-        self.pool_size = tuple(pool_size)
-        if strides is None:
-            strides = self.pool_size
-        self.strides = tuple(strides)
-        assert border_mode in {'valid', 'same'}, 'border_mode must be in {valid, same}'
-        self.border_mode = border_mode
-        assert dim_ordering in {'tf', 'th'}, 'dim_ordering must be in {tf, th}'
-        self.dim_ordering = dim_ordering
-
     @property
     def output_shape(self):
         input_shape = self.input_shape
@@ -39,10 +33,8 @@ class _Pooling2D2(Layer):
         else:
             raise Exception('Invalid dim_ordering: ' + self.dim_ordering)
 
-        rows = int(conv_output_length(rows, self.pool_size[0],
-                                  self.border_mode, self.strides[0]))
-        cols = int(conv_output_length(cols, self.pool_size[1],
-                                  self.border_mode, self.strides[1]))
+        rows = fractional_conv_output_length(rows, self.pool_size[0], self.strides[0])
+        cols = fractional_conv_output_length(cols, self.pool_size[1], self.strides[1])
 
         if self.dim_ordering == 'th':
             return (input_shape[0], input_shape[1], rows, cols)
@@ -51,29 +43,8 @@ class _Pooling2D2(Layer):
         else:
             raise Exception('Invalid dim_ordering: ' + self.dim_ordering)
 
-    def _pooling_function(self, inputs, pool_size, strides,
-                          border_mode, dim_ordering):
-        raise NotImplementedError
-
-    def get_output(self, train=False):
-        X = self.get_input(train)
-        output = self._pooling_function(inputs=X, pool_size=self.pool_size,
-                                        strides=self.strides,
-                                        border_mode=self.border_mode,
-                                        dim_ordering=self.dim_ordering)
-        return output
-
-    def get_config(self):
-        config = {'name': self.__class__.__name__,
-                  'pool_size': self.pool_size,
-                  'border_mode': self.border_mode,
-                  'strides': self.strides,
-                  'dim_ordering': self.dim_ordering}
-        base_config = super(_Pooling2D, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-class MaxPooling2D2(_Pooling2D2):
-    '''Max pooling operation for spatial data.
+class FractionalMaxPooling2D(_FractionalPooling2D):
+    '''Fractional Max pooling operation for spatial data.
     # Input shape
         4D tensor with shape:
         `(samples, channels, rows, cols)` if dim_ordering='th'
@@ -97,23 +68,20 @@ class MaxPooling2D2(_Pooling2D2):
 
     def __init__(self, pool_size=(2, 2), strides=None, border_mode='valid',
                  dim_ordering='th', **kwargs):
-        super(MaxPooling2D2, self).__init__(pool_size, strides, border_mode,
+        super(FractionalMaxPooling2D, self).__init__(pool_size, strides, border_mode,
                                            dim_ordering, **kwargs)
 
     def _pooling_function(self, inputs, pool_size, strides,
                           border_mode, dim_ordering):
-        # output = K.pool2d(inputs, pool_size, strides,
-        #                   border_mode, dim_ordering, pool_mode='max')
-        output = self.pool(inputs)
+        output = self.pool(inputs, strides)
         return output
 
-    def pool(self, inputs):
+    def pool(self, inputs, strides):
 
         _, _, n_in0, n_in1 = self.input_shape
 
-        # Hard-coded 30 for now.
-        n_out0 = 30
-        n_out1 = 30
+        n_out0 = fractional_conv_output_length(n_in0, self.pool_size[0], strides[0])
+        n_out1 = fractional_conv_output_length(n_in1, self.pool_size[1], strides[1])
 
         # Variable stride across the input creates fractional reduction
         a = theano.shared(np.array([2] * (n_in0 - n_out0) + [1] * (2 * n_out0 - n_in0)))
@@ -142,66 +110,3 @@ class MaxPooling2D2(_Pooling2D2):
         out = T.max(temp, axis=0)
 
         return out
-
-
-def theano_shuffled(input):
-    n = input.shape[0]
-    shuffled = T.permute_row_elements(input.T, _srng.permutation(n=n)).T
-    return shuffled
-
-
-
-'''
-class FractionalPool2DLayer(Layer):
-    """
-    Fractional pooling as described in http://arxiv.org/abs/1412.6071
-    Only the random overlapping mode is currently implemented.
-    """
-    def __init__(self, incoming, ds, pool_function=T.max, **kwargs):
-        super(FractionalPool2DLayer, self).__init__(incoming, **kwargs)
-        if type(ds) is not tuple:
-            raise ValueError("ds must be a tuple")
-        if (not 1 <= ds[0] <= 2) or (not 1 <= ds[1] <= 2):
-            raise ValueError("ds must be between 1 and 2")
-        self.ds = ds  # a tuple
-        if len(self.input_shape) != 4:
-            raise ValueError("Only bc01 currently supported")
-        self.pool_function = pool_function
-
-    def get_output_shape_for(self, input_shape):
-        output_shape = list(input_shape) # copy / convert to mutable list
-        output_shape[2] = int(np.ceil(float(output_shape[2]) / self.ds[0]))
-        output_shape[3] = int(np.ceil(float(output_shape[3]) / self.ds[1]))
-
-        return tuple(output_shape)
-
-    def get_output_for(self, input, **kwargs):
-        _, _, n_in0, n_in1 = self.input_shape
-        _, _, n_out0, n_out1 = self.get_output_shape()
-
-        # Variable stride across the input creates fractional reduction
-        a = theano.shared(np.array([2] * (n_in0 - n_out0) + [1] * (2 * n_out0 - n_in0)))
-        b = theano.shared(np.array([2] * (n_in1 - n_out1) + [1] * (2 * n_out1 - n_in1)))
-
-        # Randomize the input strides
-        a = theano_shuffled(a)
-        b = theano_shuffled(b)
-
-        # Convert to input positions, starting at 0
-        a = T.concatenate(([0], a[:-1]))
-        b = T.concatenate(([0], b[:-1]))
-        a = T.cumsum(a)
-        b = T.cumsum(b)
-
-        # Positions of the other corners
-        c = T.clip(a + 1, 0, n_in0 - 1)
-        d = T.clip(b + 1, 0, n_in1 - 1)
-
-        # Index the four positions in the pooling window and stack them
-        temp = T.stack(input[:, :, a, :][:, :, :, b],
-                       input[:, :, c, :][:, :, :, b],
-                       input[:, :, a, :][:, :, :, d],
-                       input[:, :, c, :][:, :, :, d])
-
-                return self.pool_function(temp, axis=0)
-'''