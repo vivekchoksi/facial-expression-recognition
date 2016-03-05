3e918790c9c157f001bea94d4b2a4053ac63f58c
Arushi Raghuvanshi <arushi.raghu@gmail.com>
2016-03-05 09:59:53 -0800

    tuned with larger depth of netowrk

--- scripts/run_baseline.py
+++ scripts/run_baseline.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#/usr/bin/python
 #
 # File: run_baseline.py
 # ---------------------
@@ -26,22 +26,26 @@ def get_server_number(counter):
 counter = 0
 
 # Generate random parameters in range
-lrs = np.random.uniform(1e-4,1e-1,2)
-regs = np.random.uniform(1e-6,1e-1,2)
-num_filters1 = random.sample(xrange(32,33), 1)
+lrs = np.random.uniform(1e-4,1e-1,3)
+regs = np.random.uniform(1e-6,1e-1,3)
+num_filters1 = random.sample(xrange(64,65), 1)
 num_filters2 = random.sample(xrange(64,65), 1)
-dropout_rates = np.random.uniform(0,1,2)
+dropout_rates = np.random.uniform(0,0.3,1)
+depth1s = random.sample(xrange(2,3), 1)
+depth2s = random.sample(xrange(2,3), 1)
 
-for nf1 in num_filters1:
-  for nf2 in num_filters2:
-    for lr in lrs:
-      for reg in regs:
-        for dr in dropout_rates:
-          # These parameters will be passed to cnn-baseline.py.
-          parameters = "-l " + str(lr) + " -r " + str(reg) + " -d " + str(dr) + " -nf1 " + str(nf1) + " -nf2 " + str(nf2) + " -nt 3000 -e 5 "
-          command = "/usr/bin/expect -f run_baseline.exp %s '%s' &" \
-            % (get_server_number(counter), parameters)
-          print 'Executing command:', command
-          os.system(command)
+for d1 in depth1s:
+  for d2 in depth2s:
+    for nf1 in num_filters1:
+      for nf2 in num_filters2:
+        for lr in lrs:
+          for reg in regs:
+            for dr in dropout_rates:
+              # These parameters will be passed to cnn-deep.py.
+              parameters = "-l " + str(lr) + " -r " + str(reg) + " -d " + str(dr) + " -nf1 " + str(nf1) + " -nf2 " + str(nf2) + " -dp1 " + str(d1) + " -dp2 " + str(d2) + " -nt 3000 -e 3 -o ./"
+              command = "/usr/bin/expect -f run_baseline.exp %s '%s' &" \
+                % (get_server_number(counter), parameters)
+              print 'Executing command:', command
+              os.system(command)
           counter += 1
           time.sleep(5)
